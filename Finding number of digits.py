ef8b680c619af3a267784dd377fd9617bb920795
Aarav Chandra <aarav@Aaravs-MBP.home>
2020-06-18 13:57:34 -0700

    added maths solution and time comparision

--- Finding number of digits.py
+++ Finding number of digits.py
@@ -1,19 +1,38 @@
+import math
+import time
+
 
 class NOD:
     def __init__(self, n):
-        self.num = n
+        self.num = abs(n)
 
-    def total_digits(self):
+    # O(digits)
+    def total_digits_iterative(self):
         c = 0
-        if(self.num == 0):
+        n = self.num
+        if(n == 0):
             return 1
-        while (self.num):
-            a = self.num % 10
+        while (n):
+            a = n % 10
             c += 1
-            self.num //= 10
+            n //= 10
         return c
+
+    # O(digits)
+    def total_digits_maths(self):
+        return(math.floor(math.log10(self.num)+1))
 
 
 n = int(input("\nPlease enter the number to check it's number of digits:\n"))
 a = NOD(n)
-print(f"{a.total_digits()}")
+start_time = time.time()
+ans = a.total_digits_iterative()
+end_time = time.time()
+t = end_time-start_time
+print(f"{ans} digits\t in {t}")
+
+start_time = time.time()
+ans = a.total_digits_maths()
+end_time = time.time()
+t = end_time-start_time
+print(f"{ans} digits\t in {t}")
